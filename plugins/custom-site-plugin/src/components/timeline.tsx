0d8315e825f620b45184f9307b613971ab2ef48a
vishals9711 <jpl-st-spring2021@sfs-wifi-dhcp-10-143-255-74.wifi.sfsu.edu>
2022-08-23 11:14:23 -0700

    Added Icons and timeline changes

--- plugins/custom-site-plugin/src/components/timeline.tsx
+++ plugins/custom-site-plugin/src/components/timeline.tsx
@@ -1,3 +1,5 @@
+/** @jsx jsx */
+import { Heading, jsx, Text } from 'theme-ui';
 import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
@@ -18,48 +20,63 @@ const SchoolIcon = () => (
   </svg>
 );
 
+const LocationIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z" /></svg>
+)
+
+const StarIcon = () => (
+  <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="StarIcon"><path d="M12 17.27 18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path></svg>
+)
+
 const timelineArray = [
   {
     icon: <SchoolIcon />,
     heading: 'Bachelor of Engineering, Computer Engineering',
     subheading: 'Goa College of Engineering',
     date: '2015 - 2019',
+    location: 'Goa, India'
   },
   {
     icon: <WorkIcon />,
     heading: 'Market Research Intern',
     subheading: 'Get My Parking',
     date: 'Jun 2017 - Jul 2017',
+    location: 'Goa, India'
   },
   {
     icon: <WorkIcon />,
     heading: 'Web Development Intern',
     subheading: 'Linker Logic Technologies',
     date: 'Jan 2018 - Feb 2018',
+    location: 'Remote'
   },
   {
     icon: <WorkIcon />,
     heading: 'Fellowship',
     subheading: 'Analog Devices',
     date: 'Feb 2018 - Aug 2018 ',
+    location: 'Bangalore, India'
   },
   {
     icon: <WorkIcon />,
     heading: 'Software Engineer',
     subheading: 'Persistent Systems',
     date: 'Jul 2019 - Jul 2021 ',
+    location: 'Goa, India'
   },
   {
     icon: <SchoolIcon />,
     heading: 'Master of Science, Computer Science',
     subheading: 'San Francisco State University',
     date: '2021 - 2023',
+    location: 'San Francisco, CA'
   },
   {
     icon: <WorkIcon />,
     heading: 'Customer Experience Intern',
     subheading: 'Privacera',
     date: 'Jun 2022 - Present',
+    location: 'Fremont, CA'
   },
 ];
 
@@ -81,10 +98,15 @@ const Timeline = () => {
             icon={data.icon}
             key={index}
           >
-            <h3 className='vertical-timeline-element-title'>{data.heading}</h3>
-            <h4 className='vertical-timeline-element-subtitle'>{data.subheading}</h4>
+            <Heading as={'h3'} className='vertical-timeline-element-title' sx={{ paddingBottom: 3, color: color }}>{data.heading}</Heading>
+            <Heading as={'h4'} className='vertical-timeline-element-subtitle'>{data.subheading}</Heading>
+            <Text sx={{ display: 'flex', marginTop: 3 }}><div sx={{ position: 'relative', top: '4px', marginRight: 2 }}><LocationIcon /></div> <span>{data.location}</span></Text>
           </VerticalTimelineElement>
         ))}
+        <VerticalTimelineElement
+            iconStyle={{ background: 'rgb(33, 150, 243)', color: color }}
+            icon={<StarIcon />}
+        />
       </VerticalTimeline>
     </div>
   );
